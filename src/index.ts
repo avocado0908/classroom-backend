166636fd59bc39b0b295740a718e44aaef01bb0c
avocado0908 <avocado0908@gmail.com>
2026-02-01 18:21:15 +1300

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const port = 8000;
 
+app.use(
+    cors({
+      origin: process.env.FRONTEND_URL, // React app URL
+      methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+      credentials: true, // allow cookies
+    })
+);
+
+
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
